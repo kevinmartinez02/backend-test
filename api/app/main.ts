b7ddb46c7fdcbd08edff0cbfa288bb08d0aeb03c
kevinmartinez02 <kemart1230@gmail.com>
2026-07-06 23:15:04 -0600

    setting up prisma with pg adapter

--- api/app/main.ts
+++ api/app/main.ts
@@ -1,8 +1,21 @@
 import express, { type Express, type Request, type Response } from 'express';
 import { config } from 'dotenv';
+import {prisma} from '../prisma/prisma.client.ts';
 const app: Express = express();
 config({path:'.env'})
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+async function testConection(){
+  try {
+    // Attempt a lightweight query to test the connection
+    console.log('conection is  up ' + await prisma.$queryRaw`SELECT 1`);
+  } catch (error) {
+    console.error('Database connection error:', error);
+  }
+}
+
 app.listen(process.env.PORT ?? '8000');
+testConection()
+console.log('server is running')
---
+++ api/prisma/prisma.client.ts
@@ -0,0 +1,8 @@
+import { config } from "dotenv"
+import {PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "@generated/client.ts"
+config({path:'.env'});
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({connectionString});
+const prisma = new PrismaClient({adapter})
+export {prisma}
